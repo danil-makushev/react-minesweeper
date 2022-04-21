0d37ba4a49c9b209563dc9499969c7b64dc17ed4
Danil Makushev <reurairin@gmail.com>
2022-04-22 02:00:30 +0300

    WIP pseudocode

--- src/minesweeper/components/PlayingField/PlayingField.tsx
+++ src/minesweeper/components/PlayingField/PlayingField.tsx
@@ -9,7 +9,8 @@ export function PlayingField(props: PlayingFieldProps) {
     return <>{generateField(props.width, props.height)}</>;
 }
 
-function generateField(width, height) {
+/** Generate empty playing field */
+function generateField(width: number, height: number) {
     const field = [];
     for (let i = 0; i < width; i++) {
         for (let j = 0; j < height; j++) {
@@ -19,3 +20,29 @@ function generateField(width, height) {
     }
     return field;
 }
+
+function setMines(
+    width: number,
+    height: number,
+    minesAmount: number,
+    startX: number,
+    startY: number
+) {
+    // randomly place {minesAmount} of mines in 2D array avoiding {startX} and {startY}
+    // calculate adjacent mines for each non-mine cell
+    // run evalTile() for the start cell
+}
+
+/** */
+function evalTile(x: number, y: number){
+    // if mine
+    // show mine, end game
+    // check neighbors
+    // if mineCount > 0
+    // show mineCount
+    // else
+}
+
+function isMine(){}
+
+function isValid(){}
